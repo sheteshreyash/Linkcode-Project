b88ce9ff8eea5b1f73ac5644709d1657214d42da
sheteshreyash <72278490+sheteshreyash@users.noreply.github.com>
2023-03-12 16:17:25 +0530

    added a commnt

---
+++ pathocare/src/test/java/com/linkcode/pathocare/PathocareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkcode.pathocare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathocareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
