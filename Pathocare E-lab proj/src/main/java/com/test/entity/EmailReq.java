1fdd7af166ed62a80c5190508dd4dc3df3121a40
sheteshreyash <72278490+sheteshreyash@users.noreply.github.com>
2023-03-16 20:32:14 +0530

    new commit

---
+++ Pathocare E-lab proj/src/main/java/com/test/entity/EmailReq.java
@@ -0,0 +1,43 @@
+package com.test.entity;
+
+public class EmailReq {
+    
+    private String to;
+    private String subject;
+    private String Message;
+
+    public EmailReq(String to, String subject, String message) {
+        super();
+        this.to = to;
+        this.subject = subject;
+        Message = message;
+    }
+
+    public EmailReq() {
+        super();
+    }
+    public String getTo() {
+        return to;
+    }
+    public void setTo(String to) {
+        this.to = to;
+    }
+    public String getSubject() {
+        return subject;
+    }
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+    public String getMessage() {
+        return Message;
+    }
+    public void setMessage(String message) {
+        Message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "EmailReq [to=" + to + ", subject=" + subject + ", Message=" + Message + "]";
+    }
+    
+}
---
+++ Pathocare E-lab proj/src/main/java/com/test/service/EmailService.java
@@ -0,0 +1,71 @@
+// package com.test.service;
+
+// import java.util.Properties;
+// import javax.mail.Message.RecipientType;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.context.MessageSourceAware;
+// import org.springframework.http.converter.smile.MappingJackson2SmileHttpMessageConverter;
+// import org.springframework.mail.javamail.MimeMailMessage;
+// import org.springframework.stereotype.Service;
+
+// import com.test.entity.EmailReq;
+
+// import jakarta.mail.Authenticator;
+// import jakarta.mail.MessageAware;
+// import jakarta.mail.PasswordAuthentication;
+// import jakarta.mail.Session;
+// import jakarta.mail.Transport;
+// import jakarta.mail.internet.InternetAddress;
+// import jakarta.mail.internet.MimeMessage;
+// import jakarta.mail.Message.RecipientType;
+
+
+// @Service
+// public class EmailService {
+    
+//     /**
+//      * @param to
+//      * @param subject
+//      * @param Message
+//      * @return
+//      */
+
+//     public boolean sendEmail(String to, String subject, String Message) {
+
+//         boolean f = false;
+//         String from = "shreyashshete02315@gmail.com";
+
+//         // variable for gmail
+//         String host = "smtp.gmail.com";
+
+//         Properties properties;
+//         Session ses = Session.getInstance(properties, new Authenticator() {
+//             @Override
+//             protected PasswordAuthentication getPasswordAuthentication() {
+//                 return new PasswordAuthentication("shreyashshete02315@gmail.com", "ikqojzkoyrozatbk");
+//             }
+//         });
+
+//         ses.setDebug(true);
+
+//         MimeMessage m = new MimeMessage(ses);
+
+//         try {
+            
+//             m.setFrom(from);
+            
+//             m.addRecipient(((RecipientType.TO)MessageAware), new InternetAddress(to));
+//             m.setSubject(subject);
+//             m.setText(Message);
+//             Transport.send(m);
+
+//             System.out.println("Sent Success....");
+//             f = true;
+
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//         }
+//         return f;
+//     }
+// }
