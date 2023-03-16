97abf6890c637734cd190caabeaa0b8e65f524a1
sheteshreyash <72278490+sheteshreyash@users.noreply.github.com>
2023-03-16 21:12:23 +0530

    new commit

--- Pathocare E-lab proj/src/main/java/com/test/entity/User.java
+++ Pathocare E-lab proj/src/main/java/com/test/entity/User.java
@@ -55,47 +55,36 @@ public class User //implements UserDetails
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getUsername() {
 		return username;
 	}
-
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
 	public String getPhoneno() {
 		return phoneno;
 	}
-
 	public void setPhoneno(String phoneno) {
 		this.phoneno = phoneno;
 	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
